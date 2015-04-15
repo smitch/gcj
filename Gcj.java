8f57991fa84d8af9b008c1d9ce593dd39a378082
smitch <kouichi.sumizaki@gmail.com>
2015-04-15 23:31:08 +0900

    added code template

---
+++ Gcj.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+public class Gcj {
+  private int T;
+  private BufferedReader buff;
+
+  private void read_args(){
+    try{
+      buff.readLine();
+    }
+    catch(Exception e){
+      System.err.println(e);
+    }
+    return;
+  }
+
+  private void solve(){
+    return;
+  }
+
+  public void run(){
+    for(int i=0; i<T; i++){
+      System.out.print(String.format("Case #%d: ", i+1));
+      read_args();
+      solve();
+    }
+    return;
+  }
+
+  Gcj(){
+    buff=new BufferedReader(new InputStreamReader(System.in));
+    try{
+      T=Integer.parseInt(buff.readLine());
+    }
+    catch(Exception e){
+      System.err.println(e);
+    }
+  }
+
+  public static final void main(final String[] args) {
+    new Gcj().run();
+    return;
+  }
+}
